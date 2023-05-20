321d07ab1eb3e51a427865d6fef8a1a5287ca408
Stormshaedow <stormshaedow@gmail.com>
2023-05-20 18:04:00 -0500

    Initial commit for Rolemaster Character Creator

---
+++ src/main/kotlin/com/example/rolemaster/character/creator/RolemasterCharacterCreatorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.rolemaster.character.creator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RolemasterCharacterCreatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<RolemasterCharacterCreatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/rolemaster/character/creator/RolemasterCharacterCreatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.rolemaster.character.creator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RolemasterCharacterCreatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
